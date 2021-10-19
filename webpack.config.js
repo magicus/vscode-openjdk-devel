db9abe0a4e8bc5ea046c5b30fd066eb4fd96935b
Magnus Ihse Bursie <mag@icus.se>
2021-10-20 00:57:52 +0200

    Fix linting/syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 //@ts-check
+/* eslint-disable max-len */
+
 
 'use strict';
 
@@ -10,7 +12,7 @@ const path = require('path');
 /** @type WebpackConfig */
 const extensionConfig = {
   target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
