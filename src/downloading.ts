938a11c06b3dc594d0102261ff9dd13567d56186
Magnus Ihse Bursie <mag@icus.se>
2023-12-03 11:55:20 +0100

    Add DownloadableContentProvider

--- src/downloading.ts
+++ src/downloading.ts
@@ -1,3 +1,5 @@
+import * as vscode from 'vscode';
+
 import fetch from 'node-fetch';
 
 export class JsonDownloader<T> {
@@ -52,6 +54,54 @@ export class JsonDownloader<T> {
           return Promise.reject(error);
         });
     } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
+
+export class DownloadableContentProvider implements vscode.TextDocumentContentProvider {
+  constructor(readonly urlPrefix: string) {
+  }
+
+  public register(context: vscode.ExtensionContext) {
+    var reg = vscode.workspace.registerTextDocumentContentProvider(this.urlPrefix, this);
+    context.subscriptions.push(reg);
+  }
+
+  public getDownloadableUrl(url: string, title: string): string {
+    return this.urlPrefix + ':' + decodeURIComponent(url) + '/name/' + title;
+  }
+
+  public openUrlInEditor(url: string, title: string) {
+    const wrappedUrl = vscode.Uri.parse(this.getDownloadableUrl(url, title));
+
+    vscode.window.showTextDocument(wrappedUrl);
+  }
+
+  private getWrappedUrl(url: string): string {
+    return decodeURIComponent(url.replace(new RegExp('^' + this.urlPrefix + ':'),
+      '').replace(/\/name\/.*$/, ''));
+  }
+
+  public provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+    const realUrl = this.getWrappedUrl(uri.toString());
+
+    return this.asyncDownload(realUrl);
+  }
+
+  private async asyncDownload(url: string): Promise<string> {
+    try {
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const fileContent = await response.text();
+        return fileContent;
+      } else {
+        console.error('Error reading from ' + url + ':', response.statusText);
+        return Promise.reject(response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching from ' + url + ':', error);
       return Promise.reject(error);
     }
   }
